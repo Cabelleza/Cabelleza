e541400d9423ba460e04246859450338cc6c079f
Julia Silva <100319104+Julialost@users.noreply.github.com>
2023-06-26 01:16:16 -0300

    adicionando os conteúdos do iframe do podcast

--- src/Components/Blog/Podcast/Podcast.jsx
+++ src/Components/Blog/Podcast/Podcast.jsx
@@ -13,12 +13,12 @@ export default function Podcast() {
                 </div>
 
                 <div className={style.containerPodcast}>
-                    <iframe  src="https://open.spotify.com/embed/track/0ups9FIuseHpBqYaEyizBS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    <iframe  src="https://open.spotify.com/embed/track/0ups9FIuseHpBqYaEyizBS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    <iframe  src="https://open.spotify.com/embed/track/0ups9FIuseHpBqYaEyizBS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    <iframe  src="https://open.spotify.com/embed/track/0ups9FIuseHpBqYaEyizBS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    <iframe  src="https://open.spotify.com/embed/track/0ups9FIuseHpBqYaEyizBS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                    <iframe  src="https://open.spotify.com/embed/track/0RnhLM38Bvg07TmQ8rCpIS?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/4b4UkjRBoZYofAosH6e2L9?utm_source=generator&t=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/1k9PytSoO3MRjVi2YUN3Em?utm_source=generator&t=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/0ZYx5GKXHRZ0HFNHNU77zv?utm_source=generator&t=5263" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/3X06rcNeHAUksVxMfR1JgT?utm_source=generator&t=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/1LvwEDgg73iPE1Iz3Kya66?utm_source=generator&t=101590" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe  src="https://open.spotify.com/embed/episode/2AWvtHWA967Nt2YAG8ZWd9?utm_source=generator&t=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                    
                 </div>
             </section>
