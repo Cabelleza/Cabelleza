456c0eed61be5a91132e97c28b2ef22e01fc1fce
unknown <abraham.keven20@gmail.com>
2023-06-28 15:47:37 -0300

    Keven - Responsividade do blog para Tablet

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import style from './Footer.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Logo from '../../imagens/logo.svg';
 import { IconContext } from "react-icons";
-import {AiOutlineInstagram, AiOutlineYoutube, AiOutlineFacebook} from "react-icons/ai";
-import {BsTelephone} from "react-icons/bs";
+import { AiOutlineInstagram, AiOutlineYoutube, AiOutlineFacebook } from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <>
             <footer>
                 <div className={style.plataforma}>
-                    <img src={Logo} alt="Logo Cabelleza" className={style.Logo}/>
+                    <img src={Logo} alt="Logo Cabelleza" className={style.Logo} />
 
                     <div>
                         <h2>Plataforma</h2>
@@ -34,27 +34,27 @@ export default function Footer(){
                     <div>
                         <h2>Contato</h2>
                         <ul>
-                            <li><a href="mailto:cabelleeza@gmail.com?subject=Informações sobre o projeto Cabelleza">cabelleeza@gmail.com</a></li> 
-                            <li><a href="tel:+5511000000000"><BsTelephone/>(11)00000-0000</a></li> 
+                            <li><a href="mailto:cabelleeza@gmail.com?subject=Informações sobre o projeto Cabelleza">cabelleeza@gmail.com</a></li>
+                            <li><a href="tel:+5511000000000"><BsTelephone />(11)00000-0000</a></li>
                         </ul>
                     </div>
                     <div>
                         <h2>Redes Sociais</h2>
-                         <ul>
+                        <ul>
                             <IconContext.Provider value={{ className: style.icons_container }}>
                                 <a href="https://www.instagram.com/cabelleza_/" target="_blank" className={style.icon}><AiOutlineInstagram /><span>Instagram</span></a>
-                                <a href="https://www.facebook.com/profile.php?id=100093713072069" target="_blank" className={style.icon}><AiOutlineFacebook /><span>Facebook</span></a> 
-                                <a href="https://www.youtube.com/@Cabelleza/featured" target="_blank" className={style.icon}><AiOutlineYoutube /><span>Youtube</span></a> 
+                                <a href="https://www.facebook.com/profile.php?id=100093713072069" target="_blank" className={style.icon}><AiOutlineFacebook /><span>Facebook</span></a>
+                                <a href="https://www.youtube.com/@Cabelleza/featured" target="_blank" className={style.icon}><AiOutlineYoutube /><span>Youtube</span></a>
                             </IconContext.Provider>
                         </ul>
                     </div>
                 </div>
-                
+
                 <div className={style.direitos}>
                     <p>Endereço: Rua Tito, nº 54 - Vila Romana, São Paulo - SP, CEP 05051-000</p>
                     <p>© Copyright 2023 - Cabelleza | Todos os direitos reservados</p>
                 </div>
-            </footer>  
+            </footer>
         </>
     )
 }
